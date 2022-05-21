8a231cd2b2c7f67538ecbb78ae53e79d316b17d9
Ahad Lag <mr.ahad.lag@gmail.com>
2022-05-21 12:05:31 +0430

    insert userListContext in app component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import Users from "./components/users";
 
+// import Contexts
+import UserListContext from './contexts/userListContext'
+
 export default function App () {
   return (
-    <Users />
+    <UserListContext.Provider value={null}>
+      <Users />
+    </UserListContext.Provider>
   )
 }
