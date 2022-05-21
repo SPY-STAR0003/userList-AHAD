cb1dc17053b51361cca45f5fbbbd1df39635763d
ahad-lag <mr.ahad.lag@gmail.com>
2022-05-21 21:07:15 +0430

    separation modal context and user list context

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -7,6 +7,7 @@ import UserEdit from "./userEdit";
 
 // import Contexts
 import UserListContext from './../../contexts/userListContext'
+import ModalContext from './../../contexts/modalContext'
 
 // import Reduces
 // import AppReducer from "./reducers/appReducer";
@@ -57,72 +58,75 @@ export default function Users (){
     }
 
     return(
-        <UserListContext.Provider value={{
-            userList,
-            setUserList,
+        <ModalContext.Provider value={{
             showModal,
             setShowModal
         }}>
-            <div className='sm:absolute w-full mt-10'>
-                <div className='sm:absolute w-full'>
-                    <div className='sm:w-2/5 reletive mx-auto text-center p-9 sm:px-14 sm:py-3 sm:bg-[#f5f7f7] bg-slate-400 border-4 border-violet-400 rounded-lg md:rounded-xl lg:rounded-2xl shadow-[0_4px_80px_0px_rgba(0,0,0,0.05)]'>
-                        <div className='text-2xl font-bold'>
-                            مدیریت کاربران
+            <UserListContext.Provider value={{
+                userList,
+                setUserList
+            }}>
+                <div className='sm:absolute w-full mt-10'>
+                    <div className='sm:absolute w-full'>
+                        <div className='sm:w-2/5 reletive mx-auto text-center p-9 sm:px-14 sm:py-3 sm:bg-[#f5f7f7] bg-slate-400 border-4 border-violet-400 rounded-lg md:rounded-xl lg:rounded-2xl shadow-[0_4px_80px_0px_rgba(0,0,0,0.05)]'>
+                            <div className='text-2xl font-bold'>
+                                مدیریت کاربران
+                            </div>
+                        </div>
+                    </div>
+                    <div className='container sm:w-4/5 w-full mx-auto'>
+                        <div className='p-6 sm:px-14 sm:pb-7 sm:pt-12 mt-7 bg-white rounded-lg md:rounded-xl lg:rounded-2xl mb-5 shadow-[0_4px_80px_0px_rgba(0,0,0,0.05)] border-2 border-violet-200'>
+                            <div className="sm:flex sm:items-center mb-4">
+                                <div className="sm:flex-auto">
+                                    <h1 className="text-xl font-semibold text-gray-900">لیست کاربران</h1>
+                                </div>
+                                <div className="mt-4 sm:mt-0 sm:flex-none">
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowModal(true)}
+                                        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                                    >
+                                        کاربر جدید
+                                    </button>
+
+                                    {/*Add modal Component*/}
+                                    <Modal
+                                        showModal={showModal}
+                                        setShowModal={setShowModal}
+                                    >
+                                        {/*send user add form in modal*/}
+                                        <UserAdd />
+                                    </Modal>
+
+                                    {/*Add Edit modal Component*/}
+                                    <Modal
+                                        showModal={showEditModal}
+                                        setShowModal={setShowEditModal}
+                                    >
+                                        {/*send user edit form in modal*/}
+                                        <UserEdit
+                                            showEditModal={showEditModal}
+                                            setShowEditModal={setShowEditModal}
+                                            userList={userList}
+                                            setUserList={setUserList}
+                                            setTargetUser={setTargetUser}
+                                            targetUser={targetUser}
+                                        />
+                                    </Modal>
+
+                                </div>
+                            </div>
+
+                            {/*Add User Search Box Component*/}
+                            <UserSearchBox searchBoxChengeHandler={searchBoxChengeHandler} />
+
+                            {/*Add User List Component*/}
+                            <UserList finalUserList={finalUserList} setUserList={setUserList} getaTargetUserHandler={getaTargetUserHandler} />
+
                         </div>
                     </div>
                 </div>
-                <div className='container sm:w-4/5 w-full mx-auto'>
-                    <div className='p-6 sm:px-14 sm:pb-7 sm:pt-12 mt-7 bg-white rounded-lg md:rounded-xl lg:rounded-2xl mb-5 shadow-[0_4px_80px_0px_rgba(0,0,0,0.05)] border-2 border-violet-200'>
-                        <div className="sm:flex sm:items-center mb-4">
-                            <div className="sm:flex-auto">
-                                <h1 className="text-xl font-semibold text-gray-900">لیست کاربران</h1>
-                            </div>
-                            <div className="mt-4 sm:mt-0 sm:flex-none">
-                                <button
-                                    type="button"
-                                    onClick={() => setShowModal(true)}
-                                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                                >
-                                    کاربر جدید
-                                </button>
-
-                                {/*Add modal Component*/}
-                                <Modal
-                                    showModal={showModal}
-                                    setShowModal={setShowModal}
-                                >
-                                    {/*send user add form in modal*/}
-                                    <UserAdd />
-                                </Modal>
-
-                                {/*Add Edit modal Component*/}
-                                <Modal
-                                    showModal={showEditModal}
-                                    setShowModal={setShowEditModal}
-                                >
-                                    {/*send user edit form in modal*/}
-                                    <UserEdit
-                                        showEditModal={showEditModal}
-                                        setShowEditModal={setShowEditModal}
-                                        userList={userList}
-                                        setUserList={setUserList}
-                                        setTargetUser={setTargetUser}
-                                        targetUser={targetUser}
-                                    />
-                                </Modal>
-
-                            </div>
-                        </div>
-
-                        {/*Add User Search Box Component*/}
-                        <UserSearchBox searchBoxChengeHandler={searchBoxChengeHandler} />
-
-                        {/*Add User List Component*/}
-                        <UserList finalUserList={finalUserList} setUserList={setUserList} getaTargetUserHandler={getaTargetUserHandler} />
-
-                    </div>
-                </div>
-            </div>
-        </UserListContext.Provider>
+            </UserListContext.Provider>
+        </ModalContext.Provider>
     )
 }
