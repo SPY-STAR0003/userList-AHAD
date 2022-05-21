9ae1e8b77c0601b744ee87da5a0b60b4cc8d272c
Ahad Lag <mr.ahad.lag@gmail.com>
2022-05-21 13:23:37 +0430

    create appReduce in reduce directory and insert reduce component in app component

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import React, { useReducer } from "react";
 import Users from "./components/users";
 
 // import Contexts
 import UserListContext from './contexts/userListContext'
+
+// import Reduces
+import AppReducer from "./reducers/appReducer";
 
 export default function App () {
   return (
---
+++ src/reducers/appReducer.js
@@ -0,0 +1,4 @@
+
+export default function AppReducer(prevState,ation){
+    return null;
+}
