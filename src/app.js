3ad98ac2f1bb3039a1aed45c96ce51dc414e5295
Ahad Lag <mr.ahad.lag@gmail.com>
2022-05-23 13:57:39 +0430

    add authenticate route (login , register) in app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React, { useReducer } from "react";
 import { Routes , Route } from 'react-router-dom';
 import Users from "./routers/users/index";
+import Authenticate from "./routers/authenticate/index";
+import Login from "./routers/authenticate/login";
+import Register from "./routers/authenticate/register";
 
 export default function App () {
   return (
     <Routes>
       <Route path="/" element={<Users />} />
+      <Route path="/auth" element={<Authenticate />} >
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
     </Routes>
   )
 }
--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,6 +1,6 @@
 
 export default function Authenticate(){
     return(
-        nall
+        <div>Authenticate</div>
     )
 }
