83ead42741227e9f92ee3d84957c597df250912f
Ahad Lag <mr.ahad.lag@gmail.com>
2022-05-23 16:03:16 +0430

    add redirect /auth to /auth/login

--- src/routers/authenticate/index.js
+++ src/routers/authenticate/index.js
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet , useNavigate , useLocation  } from "react-router-dom"
 
 export default function Authenticate(){
-    return(
-        <>
-            <div>Authenticate</div>
-            <Outlet />
-        </>
-    )
+    // create value
+    const navigatePage = useNavigate();
+    const location = useLocation();
+    // for redirect
+    if(location.pathname == '/auth/'){
+        return navigatePage("./login");
+    }else{
+        return(
+            <div className="min-h-full flex flex-col justify-center items-center py-12 sm:px-6 lg:px-8">
+                <Outlet />
+            </div>
+        );
+    }
 }
